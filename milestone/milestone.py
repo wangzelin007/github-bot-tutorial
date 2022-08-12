37da8c2de9a4c94f1254766bcfbbcc07a5561ada
ZelinWang <zelinwang@microsoft.com>
2022-08-12 10:28:36 +0800

    pull request init

---
+++ milestone/milestone.py
@@ -0,0 +1,80 @@
+import constant
+import datetime
+import logging
+import requests
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+
+def select_milestone(created_at, author, ms_type):
+    # Less than three weeks: put into the next milestone with a warning message
+    # More than three week: put into the current milestone
+    all_milestones, index = get_all_milestones()
+    current_milestone = get_current_milestone(all_milestones, index)
+    next_milestone = get_current_milestone(all_milestones, index)
+    created_at = datetime.datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%SZ')
+    if ms_type == 'issue':
+        if get_remain_days_of_current_milestone(current_milestone, created_at) > 21:
+            msg = None
+            return current_milestone, msg
+        else:
+            msg = f"Hi @{author}, Since the current milestone time is less than 21 days, " \
+                  f"this issue will be resolved in the next milestone"
+            return next_milestone, msg
+    # Less than a week: put into the next milestone with a warning message
+    # More than a week: put into the current milestone
+    if ms_type == 'pull_request':
+        if get_remain_days_of_current_milestone(current_milestone, created_at) > 7:
+            msg = None
+            return current_milestone, msg
+        else:
+            msg = f"Hi @{author}, Since the current milestone time is less than 7 days, " \
+                  f"this pr will be reviewed in the next milestone"
+            return next_milestone, msg
+
+
+def get_all_milestones():
+    # GET https://api.github.com/repos/{owner}/{repo}/milestones
+    url = 'https://api.github.com/repos/wangzelin007/github-bot-tutorial/milestones'
+    try:
+        r = requests.get(url, headers=constant.headers)
+        logger.debug('response: %s', r)
+        logger.debug('text: %s', r.text)
+        logger.debug('code: %s', r.status_code)
+    except requests.RequestException as e:
+        raise e
+    all_milestones = []
+    for i in r.json():
+        number = i['number']
+        due_on = datetime.datetime.strptime(i['due_on'], '%Y-%m-%dT%H:%M:%SZ') if i['due_on'] else None
+        all_milestones.append([number, due_on])
+    index = 0
+    for i in all_milestones:
+        if i[1] and i[1] > datetime.datetime.now():
+            break
+        else:
+            index += 1
+    logger.info('====== all_milestones %s, index: %s ======', all_milestones, index)
+    return all_milestones, index
+
+
+def get_current_milestone(all_milestones, index):
+    return all_milestones[index]
+
+
+def get_next_milestone(all_milestones, index):
+    return all_milestones[index+1]
+
+
+def get_remain_days_of_current_milestone(current_milestone, created_at):
+    # transform
+    return (current_milestone[1] - created_at).days
+
+
+if __name__=='__main__':
+    get_all_milestones()
--- pull_request/pull_request.py
+++ pull_request/pull_request.py
@@ -1,6 +1,7 @@
 import constant
 import logging
 import requests
+from milestone import milestone
 
 
 logger = logging.getLogger(__name__)
@@ -12,15 +13,47 @@ logger.addHandler(ch)
 
 def pr_opened(event):
     # common comment use issue rest api
-    # https://docs.github.com/en/rest/issues/comments
-    url = event['pull_request']['issue_url']
+    # https://api.github.com/repos/{owner}/{repo}/issues/{number}/
+    issue_url = event['pull_request']['issue_url']
+    comment_url = issue_url + '/comments'
     author = event['pull_request']['user']['login']
     message = f"Thanks for the contribution @{author}!"
     logger.info("====== message: %s ======" % message)
+    pull_request_comment(comment_url, message)
+    # get created_at
+    created_at = event['pull_request']['created_at']
+    logger.info('====== created_at: %s ======' % created_at)
+    # select milestone
+    msg, ms = milestone.select_milestone(created_at, author, ms_type='pull_request')
+    if msg:
+        pull_request_comment(comment_url, msg)
+    # assign milestone
+    # https://api.github.com/repos/{owner}/{repo}/issues/26
+    update_pull_request(issue_url, milestone=ms)
+
+
+def pull_request_comment(url, message):
     body = {
         'body': message,
     }
     try:
         r = requests.post(url, json=body, headers=constant.headers)
     except requests.RequestException as e:
+        logger.debug('response: %s', r)
+        logger.debug('text: %s', r.text)
+        logger.debug('code: %s', r.status_code)
+        raise e
+
+
+def update_pull_request(url, **kwargs):
+    body = {}
+    milestone = kwargs.pop("milestone", {})
+    if milestone:
+        body['milestone'] = milestone
+    try:
+        r = requests.post(url, json=body, headers=constant.headers)
+    except requests.RequestException as e:
+        logger.debug('response: %s', r)
+        logger.debug('text: %s', r.text)
+        logger.debug('code: %s', r.status_code)
         raise e
